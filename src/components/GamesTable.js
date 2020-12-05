8f348ec0dc04ae91098a7f9531b5190901eea5ae
jstorer <jeffrey.storer@gmail.com>
2020-12-05 12:10:11 -0500

    12/5 remove players from state

--- src/components/GamesTable.js
+++ src/components/GamesTable.js
@@ -1,51 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import GamesTableHeader from './GamesTableHeader';
-import GamesTableBody from './GamesTableBody';
-import ButtonDownloadScreenShot from './ButtonDownloadScreenshot';
-import {get, set} from '../functions/localStorage';
-import fetchGamesGHIN from '../functions/fetchGamesGHIN';
+import React from "react"
+import GamesTableHeader from "./GamesTableHeader"
+import GamesTableBody from "./GamesTableBody"
+import ButtonDownloadScreenShot from "./ButtonDownloadScreenshot"
+import { get, set } from "../functions/localStorage"
+import fetchGamesGHIN from "../functions/fetchGamesGHIN"
+import useVisibilityChange from "use-visibility-change"
 
-export default function GamesTable({handicaps}) {
-  const [players, setPlayers] = useState([]);
-  set('teesSelected', handicaps.teesSelected)
-  const dataMode = 'ghin';  
-  fetchGamesGHIN(dataMode, handicaps.players);
+export default function GamesTable({ handicaps }) {
+  set("teesSelected", handicaps.teesSelected)
+  const dataMode = "ghin"
+  fetchGamesGHIN(dataMode, handicaps.players)
+  const players = get("players")
 
-  useEffect(() => {
-  let thePlayers = get('players');
-  setPlayers(thePlayers);   
-  }, [])
-    
-  return(
-    <><br/><br/>
-      <table id='games-table'>
-      <div id='games-table-div'>
-        <thead>
-        <tr className= 'center'>
-          <th colSpan={get('teesSelected').length + 1}>
-            {handicaps.game} at {handicaps.course.toUpperCase()}
-          </th>
-        </tr>
-          <GamesTableHeader teesSelected={handicaps.teesSelected} />
-        </thead>
-        <tbody>
-          <GamesTableBody
-            players={players}
-            course={handicaps.course}
-            game={handicaps.game}
-            games={handicaps.games}
-            teesSelected={handicaps.teesSelected}
-            ratings={handicaps.ratings} 
-            slopes={handicaps.slopes} 
-            pars={handicaps.pars}/>
-        </tbody>
-      </div>
+  const onShow = () => {
+    window.location.reload()
+  }
+  useVisibilityChange({ onShow })
+
+  return (
+    <>
+      <br />
+      <br />
+      <table id="games-table">
+        <div id="games-table-div">
+          <thead>
+            <tr className="center">
+              <th colSpan={get("teesSelected").length + 1}>
+                {handicaps.game} at {handicaps.course.toUpperCase()}
+              </th>
+            </tr>
+            <GamesTableHeader teesSelected={handicaps.teesSelected} />
+          </thead>
+          <tbody>
+            <GamesTableBody
+              players={players}
+              course={handicaps.course}
+              game={handicaps.game}
+              games={handicaps.games}
+              teesSelected={handicaps.teesSelected}
+              ratings={handicaps.ratings}
+              slopes={handicaps.slopes}
+              pars={handicaps.pars}
+            />
+          </tbody>
+        </div>
       </table>
-    <br></br><br></br>
-    <div className='center'>
-    <ButtonDownloadScreenShot game={handicaps.game} course={handicaps.course.toUpperCase()} element='games-table-div' format="PNG" page="Games" />
-    </div>
+      <br></br>
+      <br></br>
+      <div className="center">
+        <ButtonDownloadScreenShot
+          game={handicaps.game}
+          course={handicaps.course.toUpperCase()}
+          element="games-table-div"
+          format="PNG"
+          page="Games"
+        />
+      </div>
     </>
   )
 }
-
